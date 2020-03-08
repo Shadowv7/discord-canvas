0ac30415915b77a8d3c787214a7e2b906039e8cb
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-03-08 10:59:20 +0400

    Update wanted.js

--- src/generate-image/wanted.js
+++ src/generate-image/wanted.js
@@ -9,6 +9,9 @@ module.exports = class Generate {
     this.background = `${__dirname}/../../assets/img/default-avatar.png`;
     /* COLOR */
     this.colorText = "#ff0000";
+    this.colorBox = "#fff";
+    /* OPACITY */
+    this.opacityBow = "0.4";
    }
   setText(value) {
   this.text = value;
@@ -23,6 +26,11 @@ module.exports = class Generate {
   if (this[formattedVariable]) this[formattedVariable] = value;
    return this;
  }
+ setOpacity(variable, value) {
+  const formattedVariable = formatVariable("opacity", variable);
+   if (this[formattedVariable]) this[formattedVariable] = value;
+     return this;
+  }
   async toAttachment() {
    const canvas = Canvas.createCanvas(1024, 1024);
    const ctx = canvas.getContext("2d");
@@ -30,6 +38,8 @@ module.exports = class Generate {
    let background = await Canvas.loadImage(this.backgroundImage);
    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
    ctx.fillRect(341,0,1024,341);
-      
+   ctx.fillStyle = this.colorBox
+   ctx.globalAlpha = this.opacityBox
+   return canvas;
  }
 }
